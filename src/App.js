b27043fae0049ba589189fb5a0f14cf65f23f974
jorge moreno <morenojorge308@gmail.com>
2022-03-09 22:23:21 -0600

    cambios contacto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component, React} from 'react';
+import {ReactDom} from 'react-dom';
+import axios  from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table,Button,Container,Modal,ModalBody,ModalHeader,FormGroup,ModalFooter} from 'reactstrap';
 
-function App() {
+const url="http://81g.itesrc.net/api/contactos";
+class App extends Component {
+  state={
+    data:[],
+    modalInsertar:false,
+    form:{
+      id:'',
+      nombre:''
+    }
+  }
+  peticionGet=()=>{
+    axios.get(url).then(response=>{this.setState({data:response.data})})
+  }
+  peticionPost=()=>{
+    axios.post(url,this.state.form).then(response=>{
+    this.modalInsertar();
+    this.peticionGet();
+    })
+    .catch
+    (
+      error=>
+      {console.log(error.message);}
+    )
+  }
+  modalInsertar=()=>{
+    this.setState({modalInsertar:!this.state.modalInsertar});
+  }
+  componentDidMount(){
+    this.peticionGet();
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+      <Button onClick={()=>this.modalInsertar()}>Agregar contacto</Button>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>nombre</th>
+          </tr>
+        </thead>
+        <tbody>
+            {this.state.data.map(contacto=>{
+            return(
+              <tr>
+                <td>{contacto.id}</td>
+                <td>{contacto.nombre}</td>
+                <td>
+                  <Button className='btn btn-primary'>Editar</Button>
+                  {"  "}
+                  <Button className='btn btn-danger'>Eliminar</Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>      
+      </table>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Tabla.jsx
@@ -0,0 +1,15 @@
+import {Component, React} from 'react';
+import {ReactDom} from 'react-dom';
+import {Table,Button,Container,Modal,ModalBody,ModalHeader,FormGroup,ModalFooter} from 'reactstrap';
+
+class Tabla extends Component{
+    render(){
+        return(
+<Table >
+
+</Table>
+
+
+        );
+    }
+}
